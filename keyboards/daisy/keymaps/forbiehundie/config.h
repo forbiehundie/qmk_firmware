a6763921a79c181a8d762b739858973d9100f0cf
forbiehundie <jmannunziata@gmail.com>
2021-02-28 15:07:32 -0700

    Initialized forbiehundie daisy keymap

---
+++ keyboards/daisy/keymaps/forbiehundie/config.h
@@ -0,0 +1,32 @@
+/*
+Copyright 2021 forbiehundie
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Tap Hold Config */
+#define TAPPING_TERM 170
+#define TAPPING_FORCE_HOLD_PER_KEY
+#define IGNORE_MOD_TAP_INTERRUPT
+//#define PERMISSIVE_HOLD
+
+/* Combos */
+#define COMBO_COUNT 4
+#define COMBO_TERM 45
+
+/* Bootmagic Lite key configuration */
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
